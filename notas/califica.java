8b4b3389a788d86da05ea8d6cbd2d86e15f33664
andreapop96 <andreaestetica26@gmail.com>
2023-02-27 21:22:11 +0100

    Nueva clase 'Califica'
    Crea una nueva clase que llamarás califica y, utilizando las funciones de refactorización, mueve el método "califica" a la nueva clase. Sube las modificaciones al repositorios de Github, siendo el comentario el enunciado de este ejercicio.

---
+++ notas/califica.java
@@ -0,0 +1,20 @@
+
+public class califica {
+	//Método para especificar la calificación en función de la nota
+	public static String califica(int num_nota){
+		String c = "";
+		if (n >= 0 && num_nota < 5)
+			c = "Suspenso";
+		else if (n >= 5 && num_nota  < 7)
+			c = "Aprobado";
+		else if (n >= 7 && num_nota  <9)
+			c = "Notable";
+		else if (n >= 9 && num_nota  <= 10)
+			c = "Sobresaliente";
+		else
+			c = "El valor de la calificación introducida no es correcta";
+
+		return c;
+			}
+	}
+
